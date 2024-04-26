c33f53bce4e125eaf0a53f05ce8e11ae6121c227
havsys <46250385+havsys@users.noreply.github.com>
2024-04-26 17:01:27 -0400

    Update windows.cmd

--- gh_actions/windows.cmd
+++ gh_actions/windows.cmd
@@ -5,8 +5,8 @@ wget https://github.com/libsdl-org/SDL/releases/download/release-2.28.2/SDL2-dev
 tar -xf SDL2-devel-2.28.2-VC.zip
 git clone -b release/30.1 --recursive https://github.com/obsproject/obs-studio.git
 move OBSPlugin\src obs-studio\plugins\ptzoptics-obs
-#patch --ignore-whitespace obs-studio\plugins\CMakeLists.txt OBSPlugin\obs_plugins_cmakelists.patch || exit /b 1
-#set SDL2_DIR=%cd%\SDL2-2.28.2
+patch --ignore-whitespace obs-studio\plugins\CMakeLists.txt OBSPlugin\obs_plugins_cmakelists.patch || exit /b 1
+set SDL2_DIR=%cd%\SDL2-2.28.2
 cd obs-studio
 cmake --build --preset windows-x64
 cd ..
