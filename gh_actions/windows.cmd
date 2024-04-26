960a40a251059e9ecedf6b688ec9488cf90c81e1
havsys <46250385+havsys@users.noreply.github.com>
2024-04-26 09:34:37 -0400

    Update windows.cmd

--- gh_actions/windows.cmd
+++ gh_actions/windows.cmd
@@ -3,12 +3,12 @@ choco install wget -y
 choco install patch -y
 wget https://github.com/libsdl-org/SDL/releases/download/release-2.28.2/SDL2-devel-2.28.2-VC.zip
 tar -xf SDL2-devel-2.28.2-VC.zip
-git clone -b release/29.1 --recursive https://github.com/obsproject/obs-studio.git
+git clone --recursive https://github.com/obsproject/obs-studio.git
 move OBSPlugin\src obs-studio\plugins\ptzoptics-obs
 patch obs-studio\plugins\CMakeLists.txt OBSPlugin\obs_plugins_cmakelists.patch || exit /b 1
 set SDL2_DIR=%cd%\SDL2-2.28.2
 cd obs-studio
-powershell.exe "CI/build-windows.ps1"
+cmake --build --preset windows-x64
 cd ..
 copy /Y obs-studio\build64\rundir\RelWithDebInfo\obs-plugins\64bit\obs-ptzcontroller.dll OBSPlugin\installers-src\win\PtzObsPluginInstaller\ || exit /b 1
 copy /Y SDL2-2.28.2\lib\x64\SDL2.dll OBSPlugin\installers-src\win\PtzObsPluginInstaller\
