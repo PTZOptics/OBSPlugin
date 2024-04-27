a3bb91c20d97bc4efb2759541957c6d15bf85d9e
havsys <46250385+havsys@users.noreply.github.com>
2024-04-26 20:51:04 -0400

    Update windows.cmd

--- gh_actions/windows.cmd
+++ gh_actions/windows.cmd
@@ -8,9 +8,10 @@ move OBSPlugin\src obs-studio\plugins\ptzoptics-obs
 patch --ignore-whitespace obs-studio\plugins\CMakeLists.txt OBSPlugin\obs_plugins_cmakelists.patch || exit /b 1
 set SDL2_DIR=%cd%\SDL2-2.28.2
 cd obs-studio
+md build_x64
 cmake --build --preset windows-x64
 cd ..
-copy /Y obs-studio\build64\rundir\RelWithDebInfo\obs-plugins\64bit\obs-ptzcontroller.dll OBSPlugin\installers-src\win\PtzObsPluginInstaller\ || exit /b 1
+copy /Y obs-studio\build_x64\rundir\RelWithDebInfo\obs-plugins\64bit\obs-ptzcontroller.dll OBSPlugin\installers-src\win\PtzObsPluginInstaller\ || exit /b 1
 copy /Y SDL2-2.28.2\lib\x64\SDL2.dll OBSPlugin\installers-src\win\PtzObsPluginInstaller\
 cd OBSPlugin\installers-src\win
 "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\amd64\MSBuild.exe" PtzObsPluginInstaller.sln /p:Configuration=Release /p:Platform=x64
